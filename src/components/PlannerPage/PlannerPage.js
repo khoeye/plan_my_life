59bab983237e62cda61fc854bff71396005b4173
khoeye <kyle.hoeye@gmail.com>
2021-12-23 20:07:21 -0800

    Added csv to planner page

--- src/components/PlannerPage/PlannerPage.js
+++ src/components/PlannerPage/PlannerPage.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import PlannerCalendar from './PlannerComps/PlannerCalendar';
 import PlannerIntro from './PlannerComps/PlannerIntro';
-// import { CSVLink } from 'react-csv';
+import { CSVLink } from 'react-csv';
 
 function PlannerPage (props){
-// Required fields include
-    // const csvData = [
-    //     {
-    //     Subject: 'Test Event 1 Name', 
-    //     StartDate: '12/22/2021', 
-    //     StartTime:'11:00 AM', 
-    //     EndDate:'12/22/2021', 
-    //     EndTime:'3:00 AM', 
-    //     AllDayEvent:'false', 
-    //     Description:'This is a test of the description', 
-    //     Location:'https://www.google.com/', 
-    //     Private:'False', }
-    //       ];
+//Required fields include
+    const csvData = [
+        {
+        Subject: 'Test Event 1 Name', 
+        StartDate: '12/22/2021', 
+        StartTime:'11:00 AM', 
+        EndDate:'12/22/2021', 
+        EndTime:'3:00 AM', 
+        AllDayEvent:'false', 
+        Description:'This is a test of the description', 
+        Location:'https://www.google.com/', 
+        Private:'False', }
+          ];
 
     //       const data = [
     //         ["firstname", "lastname", "email"],
@@ -34,7 +34,7 @@ const checkForProp = () =>{
     return (
         <div>
       <button onClick={checkForProp}>Run planner prop test</button>
-      {/* <CSVLink data={data}>Download CSV</CSVLink> */}
+      <CSVLink data={csvData}>Download CSV</CSVLink>
       <PlannerIntro/>
       <PlannerCalendar/>
       </div>
